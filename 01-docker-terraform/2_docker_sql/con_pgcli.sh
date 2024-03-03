6c6b016fa5c3b46061490638d4eb025b77f23df7
Mohamamd Alfayez <mohdhf95@gmail.com>
2024-03-03 21:43:55 +0300

    kk

---
+++ 01-docker-terraform/2_docker_sql/con_pgcli.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+pgcli -h localhost -p 5432 -u root -dny_taxi
---
+++ 01-docker-terraform/2_docker_sql/pgadmin_docker.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+docker run -it \
+    -e PGADMIN_DEFAULT_EMAIL="admin@admin.com" \
+    -e PGADMIN_DEFAULT_PASSWORD="root" \
+    -p 8080:80 \
+    dpage/pgadmin4
